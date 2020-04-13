ea52bf1a0a919459f57f3aaea7fcae10329b1b5d
maylunnalin <maylunnalin@users.noreply.github.com>
2020-04-13 15:36:43 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -11,7 +11,7 @@ int main(void)
     while (n < 1 || n > 8);
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - i; j++)
+        for (int j = 0; j < n + 1 - i; j++)
         {
             printf(" ");
         }
