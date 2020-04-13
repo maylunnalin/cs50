b5f8948f1f1ed7495c87378aef7a15d9fdc46197
maylunnalin <maylunnalin@users.noreply.github.com>
2020-04-13 14:23:09 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -11,11 +11,11 @@ int main(void)
     while (n < 1 || n > 8);
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n-1-i; j++)
+        for (int j = 0; j < n - 1 - i; j++)
         {
             printf(" ");
         }
-        for (int j = 0; j < i+1; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             printf("#");
         }
